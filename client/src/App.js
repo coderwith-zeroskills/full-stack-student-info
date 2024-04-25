78b350e5b58504a25d6ff17c107b3211bcaf7760
nikhil sacena <nikhilhill19@gmail.com>
2024-04-25 20:50:26 +0530

    changes of lcoal machine

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import StudentData from "./component/StudentData";
-
+// changes on local
 function App() {
   return (
     <div className="App">
