730a11c4dbbf6c4e1fa1b44003aed1c2f492bea6
nikhil.saxena <nikhil.saxena@softtech-engr.com>
2024-04-26 11:03:45 +0530

    500 error UI break removed.

--- server/src/api/newstudent.js
+++ server/src/api/newstudent.js
@@ -21,10 +21,6 @@ router.post("/", async (req, res) => {
     res.status(200).json({ message: "Data added successfully", status: 200 });
   } catch (error) {
     console.error("Error adding data:", error.detail);
-    res.status(500).json({
-      error: "An error occurred while adding data",
-      message: error.detail,
-    });
   }
 });
 export default router;
