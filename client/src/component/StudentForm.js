0d35aa5fd60c3d3773cd8fda9c27dd298203f761
nikhil sacena <nikhilhill19@gmail.com>
2024-05-05 21:18:29 +0530

    scope

--- client/src/component/StudentForm.js
+++ client/src/component/StudentForm.js
@@ -14,6 +14,7 @@ export default function BasicFormControl({ handleSave }) {
     age: null,
     member_parent_id: null,
   });
+  // will update UI to add more functionality
 
   return (
     <FormControl defaultValue="" required>
